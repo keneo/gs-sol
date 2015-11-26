29e2cd064f70b3ef29161fe5a52ab868a9dfe2ee
bartek <bartek@szabat.com>
2015-11-26 21:29:11 +0100

    first

---
+++ src/GsNeedsBartek/GsNeedsBartek.Tests/SeriesTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace GsNeedsBartek.Tests
+{
+    [TestFixture]
+    public class SeriesTest
+    {
+        [Test]
+        public void SeriesShouldNotContainAnyDuplicates()
+        {
+            var series = new Series().OveralSeries(0, 1, 10);
+
+
+            var duplicates = series.ToLookup(x => x).Where(e => e.Count() > 1);
+
+            duplicates.Should().BeEmpty("series should not contain any duplicates");
+        }
+
+        [Test]
+        public void SeriesShouldBeOrderedAscending()
+        {
+            var series = new Series().OveralSeries(0, 1, 10);
+           
+            series.Should().BeInAscendingOrder("series should be ordered ascending");
+        }
+
+        [Test]
+        public void NumbersInTheSeriesShouldBeRounded() //todo nearest
+        {
+            var series = new Series().OveralSeries(0, 1, 10);
+
+            
+
+            series.Should()
+                .Match(s => s.All(e =>  e%0.25f == 0), "numbers in the series should be rounded");
+
+        }
+    }
+}
---
+++ src/GsNeedsBartek/GsNeedsBartek/Series.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GsNeedsBartek
+{
+    public class Series
+    {
+        /// <summary>
+        /// a. The first number is calculated given the following function (which accepts a parameter ‘x’):
+        /// ((0.5 * x^2) + (30 * x) + 10) / 25
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public float FirstNumber(float x)
+        {
+            return -1; //todo
+        }
+
+        /// <summary>
+        /// b. A growth rate for the series is calculated using the following function (which accepts
+        /// parameters ‘y’ and the first number from a.):
+        /// (2% of y)/25/(firstNumber)
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="firstNumber"></param>
+        /// <returns></returns>
+        public float GrowRate(float y, float firstNumber)
+        {
+            return -1; //todo
+        }
+
+        /// <summary>
+        /// c. The overall series is calculated using a function that accepts three parameters
+        ////firstNumber – the number from a.
+        ////growthRate – the number from b.
+        ////length ­ the length of the resulting series
+        ////The series should start with the first number; the subsequent numbers should be calculated
+        ////as a product of:
+        ////growthRate * (firstNumber^(index of the number being generated))
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="growthRate"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public IEnumerable<float> OveralSeries(float firstNumber, float growthRate, int length)
+        {
+            yield return -1;//todo
+        }
+    }
+}
